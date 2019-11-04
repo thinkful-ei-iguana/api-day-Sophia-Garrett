4971c75eeb6a0b033f77ea15c69f661cd71d3f91
Sophia Koeut <koeutsophia@gmail.com>
2019-11-04 09:06:33 -0800

    have at it!

--- src/api.js
+++ src/api.js
@@ -1,9 +1,25 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Sophia-Garrett';
 
 const getItems = function () {
-  return Promise.resolve('A successful response!');
+  return fetch(`${BASE_URL}/items`);
 };
 
+function createItem (name){
+  let newItem = {
+    name,
+  }
+  JSON.stringify(newItem);
+  console.log(newItem);
+  return fetch(`${BASE_URL}/items`, 
+  {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: newItem,
+  });
+}
+
+
 export default {
-  getItems
+  getItems,
+  createItem
 };
